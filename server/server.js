dea304a302201a62739483003c93a6cea9e63d8e
Kayla Webb <kaylarwebb@gmail.com>
2018-02-03 17:14:36 -0500

    Implement local strategy

--- server/server.js
+++ server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 const uuid = require('uuid');
 const session = require('express-session');
 const fileStore = require('session-file-store')(session);
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
+
+const users = [{id: '2f24vvg', email: 'test@test.com', passport: 'password'}];
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  console.log('Inside local strategy callback');
+  const user = users[0];
+  if (email === user.email && password === user.password) {
+    console.log('Local strategy returned true');
+    return done(null, user);
+  }
+}
+));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.use(session({
   genid: req => {
